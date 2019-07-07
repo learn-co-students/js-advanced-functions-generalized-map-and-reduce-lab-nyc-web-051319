6c715d9e5640d5b4ace50032038bfc807ba1eb9c
github username <github email address>
2019-07-06 20:50:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(ary, f){
+  return ary.map(
+    ary => f(ary)
+  )
+}
+
+function reduce(ary, f, start=0){
+
+  return ary.reduce(
+    ((a,b) => f(a, b)), start
+  )
+}
+
+ary = [1,2,3]
+console.log(reduce(ary, function(e, memo){return e + memo}))
